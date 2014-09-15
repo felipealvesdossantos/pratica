c3bba9e4c14da5a79c001e7ade70cb5953cfafae
felipealvesdossantos <felipealvesdossantosdba@gmail.com>
2014-09-15 11:47:41 -0300

    Pratica Quartz
    Criando classes Despertador e classe de teste no junit para
    implementação e validação dos testes

---
+++ PraticaQuartz/src/DespertadorQuartz/Despertador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DespertadorQuartz;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 3db
+ */
+public class Despertador {
+    
+    public void setCallBack(){
+    }
+    
+    public void despertar(Date data){
+    }
+    
+    public void remove(){
+    }
+    
+}
